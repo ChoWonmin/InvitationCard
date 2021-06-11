78afd3cb87ff826e6f3b8dc9e8e166d18dc40938
ChoWonmin <0108257@gmail.com>
2021-06-12 01:56:34 +0900

    stars mooving

--- Moonlight/index.js
+++ Moonlight/index.js
@@ -4,3 +4,8 @@ let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
 let btn = document.getElementById('btn');
 let mountains_front = document.getElementById('mountains_front');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    stars.style.left = `${value}px`;
+})
