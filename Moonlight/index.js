a3155d12c66decb1d26f46fc891ebe3c35b6992f
ChoWonmin <0108257@gmail.com>
2021-06-15 02:15:58 +0900

    moving detail

--- Moonlight/index.js
+++ Moonlight/index.js
@@ -4,6 +4,7 @@ let mountains_behind = document.getElementById('mountains_behind');
 let text = document.getElementById('text');
 let btn = document.getElementById('btn');
 let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('header');
 
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
@@ -15,4 +16,5 @@ window.addEventListener('scroll', function() {
     text.style.marginRight = `${value * 4}px`;
     text.style.marginTop = `${value * 1.5}px`;
     btn.style.marginTop = `${value * 1.5}px`;
+    header.style.marginTop = `${value * 0.5}px`;
 })
