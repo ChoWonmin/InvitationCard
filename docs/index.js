d317761d3306a6898bb9ac04a41ca7a4db29d66e
ChoWonmin <0108257@gmail.com>
2021-12-11 22:32:42 +0900

    css transform

--- docs/index.js
+++ docs/index.js
@@ -9,12 +9,15 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', function() {
     let value = window.scrollY;
 
-    stars.style.left = `${value * 0.25}px`;
-    moon.style.top = `${value * 1.05}px`;
-    mountains_behind.style.top = `${value * 0.5}px`;
 
-    text.style.marginRight = `${value * 4}px`;
-    text.style.marginTop = `${value * 1.5}px`;
-    btn.style.marginTop = `${value * 1.5}px`;
-    header.style.marginTop = `${value * 0.5}px`;
+    stars.style.transform = `translateX(-${value * 0.25}px)`;
+
+    moon.style.transform = `translateY(${value * 1.05}px)`;
+
+    // mountains_behind.style.top = `${value * 0.5}px`;
+
+    text.style.transform = `translateX(-${value * 4}px)`;
+    // text.style.marginTop = `${value * 1.5}px`;
+    // btn.style.marginTop = `${value * 1.5}px`;
+    // header.style.marginTop = `${value * 0.5}px`;
 })
